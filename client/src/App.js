c8ea0b9491c199778dd8da8b5c381baecbb9843c
Camila Rojas <camilavaleriarojas1@gmail.com>
2022-12-31 01:14:07 -0300

    import css styles and create components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { StreamChat } from 'stream-chat';
+import { Chat } from 'stream-chat-react';
+// import Cookies from 'universal-cookie';
+
+// import ChannelContainer from './components/ChannelContainer';
+// import ChannelListContainer from './components/ChannelListContainer';
+
+import { ChannelContainer, ChannelListContainer } from './components';
+import './App.css';
+
+const apiKey = '3y78pgeurjfw';
+
+const client = StreamChat.getInstance(apiKey);
 
 const App = () => {
   return (
-    <div>
-      <h1>Chat application</h1>
+    <div className="app__wrapper">
+      <Chat client={client} theme="team light">
+        <ChannelListContainer />
+        <ChannelContainer />
+      </Chat>
     </div>
   );
 };
